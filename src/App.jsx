bd7a083a7e2a52ba0a66961383db7a845bdae28d
Nifo <60501473+nifoleyarc@users.noreply.github.com>
2025-07-05 22:53:05 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -113,7 +113,7 @@ function App() {
               {/* Header */}
               <div className="flex items-center justify-between p-4">
                 <h1 className="text-xl font-roobert-bold">
-                  💚 VODS{" "}
+                  VODS{" "}
                   <span className="text-sm font-roobert-light text-gray-400">
                     by{" "}
                     <span 
